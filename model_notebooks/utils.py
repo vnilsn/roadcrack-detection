62088b4a6352e0c2e47260b4c21417b4c7a4a357
Kohmann <eivind.kohmann@gmail.com>
2022-11-14 20:32:00 +0100

    added get_datalader function outline in utils

--- model_notebooks/utils.py
+++ model_notebooks/utils.py
@@ -1,10 +1,24 @@
 import matplotlib.pyplot as plt
 
 # colors for visualization
-COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125], [0.494, 0.184, 0.556], [0.466, 0.674, 0.188]]
+COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125], [0.494, 0.184, 0.556],
+          [0.466, 0.674, 0.188]]
+
+
+def get_datalader(path_images, path_labels=None, batch_size=32):
+    """
+    Read images and labels from the given
+    paths and return a dataloader
+    :param path_images: filepath to images
+    :param path_labels: filepath to labels, if any
+    :param batch_size:
+    :return: dataloader object ready for training / testing
+    """
+    pass
+
 
 def plot_results(image, results):
-    #plt.figure(figsize=(16,10))
+    # plt.figure(figsize=(16,10))
     plt.imshow(image)
     ax = plt.gca()
     colors = COLORS * 100
