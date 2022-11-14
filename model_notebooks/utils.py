f5912dc0e336419aa4877e19661ba717e456c85d
Kohmann <eivind.kohmann@gmail.com>
2022-11-14 23:47:25 +0100

    created function for metadata

--- model_notebooks/utils.py
+++ model_notebooks/utils.py
@@ -1,8 +1,67 @@
 import matplotlib.pyplot as plt
+import json
+import xmltodict
+from pathlib import Path
 
 # colors for visualization
 COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125], [0.494, 0.184, 0.556],
           [0.466, 0.674, 0.188]]
+
+
+def create_sample(doc):
+    """
+    Is called by create_metadata
+
+    Create a sample dictionary from the given xml file
+    :param doc: A dictionary containing all sample information
+    :return: a dictionary connecting images with objects and bounding boxes
+    """
+    doc = doc['annotation']
+    categories_names = {'D00': 0, 'D10': 1, 'D40': 2, 'D20': 3, 'pothole': 4}  # map between category name and int
+
+    if "object" not in doc:
+        return {}  # return empty dict
+
+    sample = dict()
+    sample["file_name"] = doc["filename"]
+    objects = dict()
+    bbox = list()
+    categories = list()
+
+    objs = doc["object"]
+    if isinstance(objs, dict):
+        objs = [objs]
+
+    for obj in objs:
+        cat = obj["name"]
+        bndbox = obj["bndbox"]
+        bbox.append([float(bndbox["xmin"]), float(bndbox["ymin"]), float(bndbox["xmax"]), float(bndbox["ymax"])])
+        categories.append(categories_names[cat])
+    objects["bbox"] = bbox
+    objects["categories"] = categories
+    sample["objects"] = objects
+    return sample
+
+
+def create_metadata(path_imgs, path_labels):
+    """
+    Writes a metadata file for the given images and labels
+    :param path_imgs: full path to the images
+    :param path_labels: full path to the labels
+    """
+    samples = list()
+    for file in Path(path_labels).glob('*'):
+        with open(file) as fd:
+            doc = xmltodict.parse(fd.read(), process_namespaces=True)
+            sample = create_sample(doc)
+            if sample:
+                samples.append(sample)
+
+    filename = path_imgs + "metadata.jsonl"
+    with open(filename, 'w') as f:
+        for item in samples:
+            f.write(json.dumps(item) + "\n")
+    print(f"Wrote metadata.jsonl to {path_imgs}")
 
 
 def get_datalader(path_images, path_labels=None, batch_size=32):
